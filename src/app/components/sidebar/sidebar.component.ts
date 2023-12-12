df883f8d8f43bb6688ba55a58204322f272dd8df
igntnk <ighnatenko@vk.com>
2023-12-12 12:06:23 +0300

    add photo to products

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Product } from 'src/app/model/product';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html'
 })
 export class SidebarComponent {
+
+  @Output() groupSelected = new EventEmitter();
+
+  onGroupSelectd(data: Product[]){
+    this.groupSelected.emit(data);
+  }
 
 }
--- src/app/model/product.ts
+++ src/app/model/product.ts
@@ -2,6 +2,7 @@ export class Product{
   id: BigInt;
   name: string;
   description: string;
+  photo: string;
   price: BigInt;
   enabled: boolean;
 }
